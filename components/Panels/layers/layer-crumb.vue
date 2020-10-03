27020017b83f6a6a5a35b14ec04e387907e48e64
Mattia Peiretti <mattia.peiretti@gmail.com>
2020-10-04 00:09:00 +0200

    📦 Layer editor widget core working, no styles

---
+++ components/Panels/layers/layer-crumb.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <p>{{data.name}}</p>
+        <button @click="deleteCrumb">Delete</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            data: {},
+        };
+    },
+    props: {
+        data: {
+        type: String,
+        required: true,
+        },
+    },
+    methods: {
+    deleteCrumb(e) {
+      this.$emit("deleteCrumb");
+    }
+  }
+}
+</script>
