767ab3f093bde319b468124f16ed347fdd58cd4b
Mattia Peiretti <mattia.peiretti@gmail.com>
2020-10-14 00:04:02 +0200

    💼 Textures working on 3D model

--- components/Mars/mars-globe.vue
+++ components/Mars/mars-globe.vue
@@ -6,53 +6,40 @@
 
 <script>
 import * as THREE from 'three';
-
-
 import OrbitControls from './OrbitControls';
-
-const loadTexture = () => {
-        const loader = new THREE.TextureLoader();   
-        return url => new Promise(resolve => loader.load(url, resolve));
-        }
 
 export default {
     async mounted() {
+
         const canvas = this.$el.querySelector('#canvas');
-        var scene = new THREE.Scene();
-			var camera = new THREE.PerspectiveCamera( 75, canvas.clientWidth/ canvas.clientHeight, 0.1, 1000 );
+
+        const scene = new THREE.Scene();
+			  const camera = new THREE.PerspectiveCamera( 30, canvas.clientWidth/ canvas.clientHeight, 0.1, 1000 );
+        const renderer = new THREE.WebGLRenderer({ canvas });
+
+        renderer.setSize(canvas.clientWidth, canvas.clientHeight, false);
+
+        const controls = new OrbitControls(camera, renderer.domElement);
             
+        controls.minDistance = 1.12; controls.maxDistance = 10;
+        camera.position.z = 5;
 
-            var renderer = new THREE.WebGLRenderer({ canvas });
-            renderer.setSize(canvas.clientWidth, canvas.clientHeight, false);
-			// renderer.setSize( this.$el.innerWidth, this.$el.innerHeight );
-			// this.$el.appendChild( renderer.domElement );
+        const loader = new THREE.TextureLoader();
+        const map = loader.load(require('@/static/textures/2k_mars.jpg'));
 
-            const controls = new OrbitControls(camera, renderer.domElement);
-            controls.minDistance = 1.12; controls.maxDistance = 10;
+        scene.add(new THREE.Mesh(new THREE.SphereBufferGeometry(1, 32, 32), new THREE.MeshBasicMaterial({map})));
 
-            camera.position.z = 5;
-
-
-			// var animate = function () {
-			// 	requestAnimationFrame( animate );
-
-			// 	cube.rotation.x += 0.01;
-			// 	cube.rotation.y += 0.01;
-
-			// 	renderer.render( scene, camera );
-			// };
-
-            // animate();
-
-            const map = await loadTexture(`https://solartextures.b-cdn.net/2k_mars.jpg`);
-            scene.add(new THREE.Mesh(new THREE.SphereBufferGeometry(1, 32, 32), ));
-
-            renderer.render(scene, camera);
-            controls.addEventListener("change", () => renderer.render(scene, camera));
-            
-
-                    
-
+        
+        controls.addEventListener("change", () => renderer.render(scene, camera));
+        renderer.render(scene, camera);
     }
 }
 </script>
+
+<style scoped>
+
+#canvas {
+  cursor:move
+}
+
+</style>
