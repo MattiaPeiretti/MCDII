576a796ec47a70a13b86a5d0ce2320a9bdff2633
Mattia Peiretti <mattia.peiretti@gmail.com>
2020-10-20 21:24:07 +0200

    🐜 [BUG FIX] 3D mesh not showing until first interaction with orbitControl

--- components/Mars/mars-globe.vue
+++ components/Mars/mars-globe.vue
@@ -5,41 +5,58 @@
 </template>
 
 <script>
-import * as THREE from 'three';
-import OrbitControls from './OrbitControls';
+import * as THREE from "three";
+import OrbitControls from "./OrbitControls";
 
 export default {
-    async mounted() {
-
-        const canvas = this.$el.querySelector('#canvas');
-
-        const scene = new THREE.Scene();
-			  const camera = new THREE.PerspectiveCamera( 30, canvas.clientWidth/ canvas.clientHeight, 0.1, 1000 );
-        const renderer = new THREE.WebGLRenderer({ canvas });
-
-        renderer.setSize(canvas.clientWidth, canvas.clientHeight, false);
-
-        const controls = new OrbitControls(camera, renderer.domElement);
-            
-        controls.minDistance = 1.12; controls.maxDistance = 10;
-        camera.position.z = 5;
-
-        const loader = new THREE.TextureLoader();
-        const map = loader.load(require('@/static/textures/2k_mars.jpg'));
-
-        scene.add(new THREE.Mesh(new THREE.SphereBufferGeometry(1, 32, 32), new THREE.MeshBasicMaterial({map})));
-
-        
-        controls.addEventListener("change", () => renderer.render(scene, camera));
+  async mounted() {
+    const loader = new THREE.TextureLoader();
+    const map = await loader.load(
+      require("@/static/textures/2k_mars.jpg"),
+      () => {
         renderer.render(scene, camera);
-    }
-}
+      }
+    );
+    const canvas = this.$el.querySelector("#canvas");
+    const w = canvas.clientWidth / 1.5,
+      h = canvas.clientHeight / 1.5;
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(
+      30,
+      canvas.clientWidth / canvas.clientHeight,
+      0.1,
+      1000
+    ).translateZ(5);
+
+    const renderer = new THREE.WebGLRenderer({ canvas });
+
+    renderer.setSize(canvas.clientWidth, canvas.clientHeight, false);
+
+    // let globe = new THREE.Mesh(new THREE.SphereBufferGeometry(1, 32, 32), new THREE.MeshBasicMaterial({color: 0xffffff}));
+    let globe = new THREE.Mesh(
+      new THREE.SphereBufferGeometry(1, 32, 32),
+      new THREE.MeshBasicMaterial({ map })
+    );
+
+    scene.add(globe);
+
+    renderer.render(scene, camera);
+
+    const controls = new OrbitControls(camera, renderer.domElement);
+
+    controls.minDistance = 1.12;
+    controls.maxDistance = 10;
+
+    controls.update();
+
+    controls.addEventListener("change", () => renderer.render(scene, camera));
+    renderer.render(scene, camera);
+  }
+};
 </script>
 
 <style scoped>
-
 #canvas {
-  cursor:move
+  cursor: move;
 }
-
 </style>
