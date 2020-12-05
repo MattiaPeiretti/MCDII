12486b7fdee578380cd42f8abe826d7e832c7014
Mattia Peiretti <mattia.peiretti@gmail.com>
2020-12-05 22:42:09 +0100

    📦 Added info word button

---
+++ components/Buttons/info-text.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="container-info-text" @click="clicked()">
+        <p><slot></slot>&#128712; </p>
+    </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        clicked() {
+            this.$emit("clicked")
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    .container-info-text {
+        display: inline-block;
+        p {
+            color: gray;
+            padding-right: 5px;
+            cursor: help;
+        }
+        margin: 0!important;
+        padding: 0!important;
+    }
+</style>
